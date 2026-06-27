c49530e4f73c3cbb984a104a9dfb0121731fb3a2
jhan0121 <jhan0121@gmail.com>
2026-06-27 15:10:52 +0900

    init: create project

---
+++ src/main/kotlin/org/flinter/aichatbot/AiChatbotApplication.kt
@@ -0,0 +1,11 @@
+package org.flinter.aichatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatbotApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiChatbotApplication>(*args)
+}
